79237f959372b6127ec9550bd9a76b8a65fdcb6c
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-08-25 16:22:06 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,12 +5,12 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
-  def upload 
+  def upload
     CSV.foreach(params['file'].path, headers: true) do |song|
       Song.create(title: song[0], artist_name: song[1])
-    end 
+    end
     redirect_to songs_path
-  end 
+  end
 
   def show
     @song = Song.find(params[:id])
@@ -59,4 +59,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
